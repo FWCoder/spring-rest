36692a5183c91d37258e2e1595233d208f5608af
Brian Nguyen <brnguyen@brnguyen.remote.csb>
2019-09-12 10:33:27 -0400

    Update calling program with bascript

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 EXPOSE 8080
 COPY target/shift-rest-1.0.0-SNAPSHOT.jar app.jar
+COPY startup.sh startup.sh
 RUN apk --no-cache add curl
 #ENTRYPOINT ["java", "-jar", "app.jar", "--person.name=Brian"]
-ENTRYPOINT ["java", "-jar", "app.jar"]
+ENTRYPOINT ["/bin/sh", "startup.sh"]
--- deploy-app.sh
+++ deploy-app.sh
@@ -1,12 +1,20 @@
 mvn clean install
-oc delete all -l app=spring-test
-oc delete secret spring-test
+oc delete all --all=true
 
 oc create secret generic spring-test --from-literal=name=DK --output=yaml --dry-run > spring-test.yaml
 oc create -f spring-test.yaml
 oc process -f spring-test-template.yml | oc apply -f -
-#oc create route edge spring-test-v0 --service=spring-test --hostname=springtest-myproject2.192.168.42.110.nip.io --insecure-policy Allow
 oc set env dc spring-test SERVER_SERVLET_CONTEXT_PATH="/v1"
-oc create route edge spring-test-v1 --service=spring-test --hostname=springtest-myproject2.192.168.42.110.nip.io --path /v1 --insecure-policy Allow
+oc create route edge spring-test --service=spring-test --hostname=springtest-myproject2.192.168.42.110.nip.io --path /v1 --insecure-policy Allow
 oc new-build --strategy docker --binary --docker-image openjdk:8-jdk-alpine --name spring-test -l app=spring-test
 oc start-build spring-test --from-dir=.
+
+while :
+do 
+ echo "waiting for pod to be complete"
+ pod_status=$(oc get pods -l app=spring-test -o=jsonpath='{.items[*].status.containerStatuses[0].ready}')
+ if [ "$pod_status" == "true" ]
+ then
+  break;
+ fi
+done
